96d608ac5f3935d76a780a75cf28c3cfe2d35999
jcobu <24739101+jcobu@users.noreply.github.com>
2021-11-24 13:06:51 -0800

    Created a menu and started work on the automated scanning method

--- historyCord.py
+++ historyCord.py
@@ -17,18 +17,47 @@ class urlTemplate:
         urlPrefix = 'https://media.discordapp.net/attachments'
         return (f'{urlPrefix}/{self.uuid}/{self.uuid2}/{self.fname}')
 
-# Main url building method
+
+# Main menu
+def menu():
+    print('(1) for manual')
+    print('(2) for automatic sifting')
+    selection = input('Selection: ')
+
+    if selection == '1':
+        urlBuilder()
+
+    elif selection == '2':
+        automatedBuilder()
+
+    else:
+        exit()
+
+# Manual search
 def urlBuilder():
-
     print('Example: https://media.discordapp.net/attachments/<useruuid>/<secondaryuuid>/<unknown.png>')
-
     userInput = urlTemplate(input('UUID: '), input('Secondary UUID: '), input('File name (if known): '))
-
     resp = requests.get(userInput.combinedURL())
-
     print(resp.status_code)
-
     # Clickable link to view the image
     print(userInput.combinedURL())
 
-urlBuilder()
+# Automated search based on wordlists
+def automatedBuilder():
+    # Open pre-generated 6 digit list
+    with open('numbers.txt') as n:
+        alist = [line.rstrip() for line in n]
+        for a in list:
+            blist = []
+            blist.append(a)
+            #combinedNumbers
+
+        # Snap in the numbers
+        '''
+        automatedInput = urlTemplate('232323', '232323', 'unknown.png')
+        resp = requests.get(automatedInput.combinedURL())
+        print(resp.status_code)
+        '''
+    n.close()
+
+menu()
