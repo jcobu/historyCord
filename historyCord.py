c867ad196694a4091f32ca62db54563ea704de07
jcobu <24739101+jcobu@users.noreply.github.com>
2021-11-24 08:35:56 -0800

    first commit

---
+++ historyCord.py
@@ -0,0 +1,11 @@
+"""
+Discord image history viewer
+Attempts to guessimate unique ID keyspace in image URIs
+Author: jcobu
+"""
+
+def main():
+    print("First commit")
+
+if __name__ == "__main__":
+    main()
