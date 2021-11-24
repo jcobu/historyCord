e8e824d394b4753183ad5c6fa0aa81e8edfb408b
jcobu <24739101+jcobu@users.noreply.github.com>
2021-11-24 12:05:36 -0800

    Added some actual functionality

--- historyCord.py
+++ historyCord.py
@@ -1,11 +1,34 @@
-"""
+'''
 Discord image history viewer
-Attempts to guessimate unique ID keyspace in image URIs
-Author: jcobu
-"""
+Attempts to guessimate unique ID keyspace in image URIs that are publically available
+Author: @jcobu
+'''
 
-def main():
-    print("First commit")
+import requests
 
-if __name__ == "__main__":
-    main()
+class urlTemplate:
+
+    def __init__(self, uuid, uuid2, fname):
+        self.uuid = uuid
+        self.uuid2 = uuid2
+        self.fname = fname
+
+    def combinedURL(self):
+        urlPrefix = 'https://media.discordapp.net/attachments'
+        return (f'{urlPrefix}/{self.uuid}/{self.uuid2}/{self.fname}')
+
+# Main url building method
+def urlBuilder():
+
+    print('Example: https://media.discordapp.net/attachments/<useruuid>/<secondaryuuid>/<unknown.png>')
+
+    userInput = urlTemplate(input('UUID: '), input('Secondary UUID: '), input('File name (if known): '))
+
+    resp = requests.get(userInput.combinedURL())
+
+    print(resp.status_code)
+
+    # Clickable link to view the image
+    print(userInput.combinedURL())
+
+urlBuilder()
