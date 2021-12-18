144206f89429fc4dc868999828f2f5a5ca56757a
jcobu <24739101+jcobu@users.noreply.github.com>
2021-12-17 16:59:19 -0800

    Added more functionality by creating a combiner function

--- historyCord.py
+++ historyCord.py
@@ -4,7 +4,8 @@ Attempts to guessimate unique ID keyspace in image URIs that are publically avai
 Author: @jcobu
 '''
 
-import requests
+import requests, random
+foundList = []
 
 class urlTemplate:
 
@@ -15,8 +16,9 @@ class urlTemplate:
 
     def combinedURL(self):
         urlPrefix = 'https://media.discordapp.net/attachments'
-        return (f'{urlPrefix}/{self.uuid}/{self.uuid2}/{self.fname}')
-
+        urlComplete = (f'{urlPrefix}/{self.uuid}/{self.uuid2}/{self.fname}')
+        print(urlComplete)
+        return urlComplete
 
 # Main menu
 def menu():
@@ -25,39 +27,51 @@ def menu():
     selection = input('Selection: ')
 
     if selection == '1':
-        urlBuilder()
-
+        manualBuilder()
     elif selection == '2':
         automatedBuilder()
-
     else:
         exit()
 
+# Picks random 3, 6 digit numbers and appends them to a single str var
+def combiner():
+    # Open pre-generated 6 digit list
+    lines = open('numbers.txt').read().splitlines()
+    list = str(random.choice(lines))
+    return list
+    # Closes the file
+    lines.close()
+
 # Manual search
-def urlBuilder():
+def manualBuilder():
+    alist = ''
+    for x in range(3):
+        alist += combiner()
+
     print('Example: https://media.discordapp.net/attachments/<useruuid>/<secondaryuuid>/<unknown.png>')
-    userInput = urlTemplate(input('UUID: '), input('Secondary UUID: '), input('File name (if known): '))
+    userInput = urlTemplate(input('UUID: '), alist, input('File name (if known): '))
     resp = requests.get(userInput.combinedURL())
     print(resp.status_code)
-    # Clickable link to view the image
-    print(userInput.combinedURL())
 
 # Automated search based on wordlists
 def automatedBuilder():
-    # Open pre-generated 6 digit list
-    with open('numbers.txt') as n:
-        alist = [line.rstrip() for line in n]
-        for a in list:
-            blist = []
-            blist.append(a)
-            #combinedNumbers
+    for z in range(4):
+        alist = ''
+        blist = ''
+        for x in range(3):
+            alist += combiner()
+            blist += combiner()
 
-        # Snap in the numbers
         '''
-        automatedInput = urlTemplate('232323', '232323', 'unknown.png')
+        # Debug printing
+        print(f'alist: {alist}')
+        print(f'blist: {blist}')
+        '''
+        
+        # Snap in the numbers uses classes idk lol
+        automatedInput = urlTemplate(alist, blist, 'unknown.png')
         resp = requests.get(automatedInput.combinedURL())
+
         print(resp.status_code)
-        '''
-    n.close()
 
 menu()
