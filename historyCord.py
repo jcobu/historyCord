31acda21ceb4d6fd5781e7cc71db8d58e44b92a9
jcobu <24739101+jcobu@users.noreply.github.com>
2021-12-28 12:09:20 -0800

    Added more functionality

--- historyCord.py
+++ historyCord.py
@@ -3,7 +3,7 @@ Discord image history viewer
 Attempts to guessimate unique ID keyspace in image URIs that are publically available
 Author: @jcobu
 '''
-
+from sys import exit
 import requests, random
 foundList = []
 
@@ -20,18 +20,34 @@ class urlTemplate:
         print(urlComplete)
         return urlComplete
 
+# Temporary array keeping track of valid urls
+def ifFound(code, url):
+    if code == 200:
+        for a in foundList:
+            if url != a:
+                foundList.append(url)
+
+# Prints the final foundList array vertically
+def printStuff():
+    for i in foundList:
+        print('Valid links: ' + i)
+
 # Main menu
 def menu():
-    print('(1) for manual')
-    print('(2) for automatic sifting')
-    selection = input('Selection: ')
+    while(True):
+        print('(1) for manual')
+        print('(2) for automatic sifting')
+        selection = input('Selection: ')
 
-    if selection == '1':
-        manualBuilder()
-    elif selection == '2':
-        automatedBuilder()
-    else:
-        exit()
+        if selection == '1':
+            print('Starting manual\n')
+            manualBuilder()
+        elif selection == '2':
+            print('Starting automatic\n')
+            automatedBuilder()
+        else:
+            print('Exiting\n')
+            exit(0)
 
 # Picks random 3, 6 digit numbers and appends them to a single str var
 def combiner():
@@ -44,34 +60,40 @@ def combiner():
 
 # Manual search
 def manualBuilder():
-    alist = ''
-    for x in range(3):
-        alist += combiner()
-
     print('Example: https://media.discordapp.net/attachments/<useruuid>/<secondaryuuid>/<unknown.png>')
-    userInput = urlTemplate(input('UUID: '), alist, input('File name (if known): '))
-    resp = requests.get(userInput.combinedURL())
-    print(resp.status_code)
+    userUUID = input('User UUID: ')
+    userFile = input('File name (if known): ')
+    if userFile == '':
+        userFile = 'unknown.png'
+
+    for a in range(10):
+        alist = ''
+        for x in range(3):
+            alist += combiner()
+
+        # Combines the provided info
+        userInput = urlTemplate(userUUID, '909926715197902868', userFile)
+        resp = requests.get(userInput.combinedURL())
+        print(resp.status_code)
+        ifFound(resp.status_code, userInput.combinedURL())
+
+        printStuff()
 
 # Automated search based on wordlists
 def automatedBuilder():
-    for z in range(4):
+    for z in range(10):
         alist = ''
         blist = ''
         for x in range(3):
             alist += combiner()
             blist += combiner()
-
-        '''
-        # Debug printing
-        print(f'alist: {alist}')
-        print(f'blist: {blist}')
-        '''
         
-        # Snap in the numbers uses classes idk lol
+        # Combines the provided info using classes
         automatedInput = urlTemplate(alist, blist, 'unknown.png')
         resp = requests.get(automatedInput.combinedURL())
-
         print(resp.status_code)
+        ifFound(resp.status_code, automatedInput.combinedURL())
+
+        printStuff()
 
 menu()
